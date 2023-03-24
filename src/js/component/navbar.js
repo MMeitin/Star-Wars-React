fac8c11bbeba08b799f2b322df08c8fbf0c18591
MMeitin <mario.meitin.alvarez@gmail.com>
2023-03-24 15:54:11 +0000

    feat: Add Star Wars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { Context } from "../store/appContext";
+
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  const { store, actions } = useContext(Context);
+
+  return (
+    <nav className="navbar navbar-light bg-light m-3">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">
+          <i className="fa-solid fa-jedi"></i>
+        </span>
+      </Link>
+      <div className="ml-auto">
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-primary dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Favorites {store.favs.length}
+          </button>
+          <ul className="dropdown-menu dropdown-menu-end">
+            {store.favs.map((item, index) => (
+              <li key={index} className="dropdown-item">
+                <div className="d-flex justify-content-between align-items-center">
+                  <p className="m-2">{item}</p>
+                  <button
+                    onClick={() => actions.removeFav(item)}
+                    className="btn btn-danger"
+                  >
+                    X
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,57 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 import "../../styles/home.css";
+import { fetchData } from "../services/index.js";
+import Card from "../component/card.jsx";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+
+  const [chars, setChars] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [vehicles, setVehicles] = useState([]);
+
+
+  const getCharsFromFetch = async () => {
+    const data = await fetchData("people");
+    setChars(data.results);
+  };
+  const getPlanetsFromFetch = async () => {
+    const data = await fetchData("planets");
+    setPlanets(data.results);
+  };
+  const getVehiclesFromFetch = async () => {
+    const data = await fetchData("vehicles");
+    setVehicles(data.results);
+  };
+
+  useEffect(() => {
+    getCharsFromFetch();
+    getPlanetsFromFetch();
+    getVehiclesFromFetch();
+  }, []);
+
+  return (
+    <div className="mt-5">
+      <div className="container d-flex justify-content-center mx-5">
+          <h1>Characters</h1>
+        <div className="chars d-flex flex-nowrap gap-2">
+          {chars.map((elem, i) => (
+            <Card key={i} index={i} uid={elem.uid} name={elem.name} var='people' img='characters' />
+          ))}
+        </div>
+          <h1>Planets</h1>
+        <div className="planets d-flex flex-nowrap gap-2">
+          {planets.map((elem, i) => (
+            <Card key={i} index={i}  uid={elem.uid} name={elem.name} var= 'planets' img='planets' />
+          ))}
+        </div>
+          <h1>Vehicles</h1>
+        <div className="vehicles d-flex flex-nowrap gap-2">
+          {vehicles.map((elem, i) => (
+            <Card key={i} index={i}  uid={elem.uid} name={elem.name} var= 'vehicles' img='vehicles' />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
